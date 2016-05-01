903f93e7b074c6e093d4d94dbc397c6ade47651f
Alex <actsang@vm132.sysnet.ucsd.edu>
2016-05-01 02:07:35 -0700

    Adding code

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID="AKIAJIL4IVPNC7TI536A"
-export AWS_SECRET_ACCESS_KEY="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"
-export ROLE="arn:aws:iam::751324341733:role/lambda"
-export LAMBDA_NAME="testonemore"
+export AWS_ACCESS_KEY_ID="AKIAJKOXPLIICPXUIFKQ"
+export AWS_SECRET_ACCESS_KEY="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"
+export ROLE="arn:aws:iam::751324341733:role/lambdaPermissions"
+export LAMBDA_NAME="testtwomore"
