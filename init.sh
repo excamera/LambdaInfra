7454eecd6a88f6932bae4014078e752cba350f36
Ubuntu <ubuntu@ip-172-31-34-157.eu-west-1.compute.internal>
2016-05-01 07:35:03 +0000

    Removing unwanted filed

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export AWS_ACCESS_KEY_ID="AKIAJIL4IVPNC7TI536A"
-export AWS_SECRET_ACCESS_KEY="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"
-export ROLE="arn:aws:iam::751324341733:role/lambda"
-export LAMBDA_NAME="testonemore"
